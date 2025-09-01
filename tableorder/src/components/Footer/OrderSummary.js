1d153a46e3fa9d6909264823e777de82285bab1c
nonamehj <wpgus77@naver.com>
2025-09-01 14:39:19 +0900

    Updates

---
+++ tableorder/src/components/Footer/OrderSummary.js
@@ -0,0 +1,55 @@
+import React, { useState, useCallback } from "react";
+import { useGlobalContext } from "../../context";
+import "./OrderSummaryStyle.css";
+import Alert from "./../Modal/Alert/Alert";
+import {
+  AiOutlineShoppingCart,
+  AiOutlineFileText,
+  AiOutlineBell,
+} from "react-icons/ai";
+const AddItems = () => {
+  const { openCart, cartItem, cartAmount, showOrderDetails, showAlertModal } =
+    useGlobalContext();
+  const [isShaking, setIsShaking] = useState(false);
+  // const handleClick = useCallback(() => {
+  //   setIsShaking(true);
+  //   showAlertModal();
+  //   setTimeout(() => {
+  //     setIsShaking(false);
+  //   }, 2000);
+  // }, [setIsShaking, showAlertModal]);
+
+  return (
+    <footer>
+      <div className="footer-container">
+        <button
+          className={`call-btn ${isShaking ? "shaking-button" : ""}`}
+          // onClick={handleClick}
+          onClick={showAlertModal}
+        >
+          <AiOutlineBell className="icon" />
+          <span>호출</span>
+        </button>
+        <div className="footer-buttons">
+          <button className="left-btn cart-order" onClick={openCart}>
+            <AiOutlineShoppingCart className="icon" />
+            <span>장바구니</span>
+            {cartItem.length > 0 && (
+              <span className="cart-amount">{cartAmount}</span>
+            )}
+          </button>
+
+          <button
+            className="right-btn order-details"
+            onClick={showOrderDetails}
+          >
+            <AiOutlineFileText className="icon" />
+            <span>주문내역</span>
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default AddItems;
